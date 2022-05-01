28544e98b4165208978255bc509605746bbb1855
Abhishek kumar singh <82291395+abhigeu9193@users.noreply.github.com>
2022-05-01 18:46:54 +0530

    Update Program.cs

--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -1,8 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
-// C# program to check
-// for a leap year
 using System;
 
 class LeapYear
