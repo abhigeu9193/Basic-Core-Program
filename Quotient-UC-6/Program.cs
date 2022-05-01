dbc2f0da2596b3986dd5c24a956ebb97dc133a69
Abhishek <abhigeu9193@gmail.com>
2022-05-01 22:16:43 +0530

    Quotient and Remainder

---
+++ Quotient-UC-6/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+public class Test
+{
+	public static void Main(string[] args)
+	{
+		int x, y, q, r;
+		Console.Write("Enter first number:");
+		x = Convert.ToInt32(Console.ReadLine());
+
+		Console.Write("Enter second number:");
+		y = Convert.ToInt32(Console.ReadLine());
+
+		q = x / y;
+		r = x % y;
+
+		Console.WriteLine("Quotient is:" + q);
+		Console.WriteLine("Remainder is:" + r);
+
+	}
+}
