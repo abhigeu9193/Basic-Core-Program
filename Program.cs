0b274eca27b4abb98f61101cbbb10d9313da9ccb
Abhishek kumar singh <82291395+abhigeu9193@users.noreply.github.com>
2022-05-01 22:26:40 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+
 using System;
-public class Test
+public class SwapExample
 {
-	public static void Main(string[] args)
-	{
-		int x, y, q, r;
-		Console.Write("Enter first number:");
-		x = Convert.ToInt32(Console.ReadLine());
-
-		Console.Write("Enter second number:");
-		y = Convert.ToInt32(Console.ReadLine());
-
-		q = x / y;
-		r = x % y;
-
-		Console.WriteLine("Quotient is:" + q);
-		Console.WriteLine("Remainder is:" + r);
-
-	}
+    public static void Main(string[] args)
+    {
+        int a = 20, b = 30;
+        Console.WriteLine("Before swap a= " + a + " b= " + b);
+        a = a + b;      
+        b = a - b;     
+        a = a - b;   
+        Console.Write("After swap a= " + a + " b= " + b);
+    }
 }
