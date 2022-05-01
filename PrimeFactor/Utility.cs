bb62e88892af22de4a24a922628c25d4cc237eda
Abhishek <abhigeu9193@gmail.com>
2022-05-01 19:24:27 +0530

    Prime Factor

---
+++ PrimeFactor/Utility.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+internal class Utility
+{
+    public Utility()
+    {
+    }
+
+    internal int inputInteger()
+    {
+        throw new NotImplementedException();
+    }
+}
