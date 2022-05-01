d66441cc130a9d4edac2973906327dcd669829af
Abhishek <abhigeu9193@gmail.com>
2022-05-01 20:44:43 +0530

    PowerOfTwo

---
+++ Power/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// C# code to find highest power
+// of 2 smaller than or equal to n.
+using System;
+
+class Power
+{
+	public static int Powerof2(int n)
+	{
+		int res = 0;
+		for (int i = n; i >= 1; i--)
+		{
+			// If i is a power of 2
+			if ((i & (i - 1)) == 0)
+			{
+				res = i;
+				break;
+			}
+		}
+		return res;
+	}
+
+	// Driver Code
+	static public void Main()
+	{
+		int n = 8;
+		Console.WriteLine(Powerof2(n));
+	}
+}
+
